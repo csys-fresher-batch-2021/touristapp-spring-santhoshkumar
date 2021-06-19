4c51cde0339cb1cb6cc3c5085cf7313da5d4f6c1
Santhosh Kumar B <santhoshb0899@gmail.com>
2021-06-19 11:00:55 +0530

    initial commit

---
+++ src/main/java/in/santhoshkumar/TouristappApiSanthoshApplication.java
@@ -0,0 +1,13 @@
+package in.santhoshkumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristappApiSanthoshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristappApiSanthoshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/santhoshkumar/TouristappApiSanthoshApplicationTests.java
@@ -0,0 +1,13 @@
+package in.santhoshkumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristappApiSanthoshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
